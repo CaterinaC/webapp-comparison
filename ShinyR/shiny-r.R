b88895274477600a1a2f330c1762d88118c183e8
CaterinaC <caterina.constantinescu@gmail.com>
2022-09-27 17:15:03 +0100

    Path change and minor code simplification

--- ShinyR/shiny-r.R
+++ ShinyR/shiny-r.R
@@ -1,4 +1,3 @@
-
 library(dplyr)
 library(shiny)
 library(shinyWidgets)
@@ -7,7 +6,7 @@ library(data.table)
 library(magrittr)
 library(prophet)
 
-dt <- fread("/home/caterina/Documents/TalksJune2020Onwards/ShinyVsPythonCompetitors/code/ShinyR/data/NEED_data_explorer_2021.csv",
+dt <- fread("/home/caterina/Documents/TalksJune2020Onwards/ShinyVsPythonCompetitors/code/webapp-comparison/ShinyR/data/NEED_data_explorer_2021.csv",
             na.strings = "n/a")
 
 REGIONS <- c("North East", 
@@ -40,7 +39,6 @@ ELEC_DIMS <- grep("Elec Median", names(dt), value =  TRUE)
 ui <- pageWithSidebar(
   headerPanel('Test app'),
   sidebarPanel(
-    
     pickerInput(inputId = "energy_type", 
                 label = "Energy type:", 
                 choices = c("Electricity", "Gas"), 
@@ -61,8 +59,8 @@ ui <- pageWithSidebar(
 
 
 
+
 server <- function(input, output, session) {
-  
   
   selectedData <- reactive({
     
@@ -72,36 +70,29 @@ server <- function(input, output, session) {
                         "Attribute 2",
                         GAS_DIMS), 
                       with = FALSE]
-      dt_chunk <- melt(dt_chunk, measure.vars = sort(GAS_DIMS))
-      
-      dt_chunk[ , variable := gsub("Gas Median ", "", variable) %>% as.numeric()]
-      
-      dt_chunk[ , value := as.numeric(value)]
-      
+
+
     } else if (input$energy_type == "Electricity") {
       dt_chunk <- dt[ `Attribute 1` %in% input$Id008 & `Attribute 2` %in% PERIODS, 
                       c("Attribute 1",
                         "Attribute 2",
                         ELEC_DIMS), 
                       with = FALSE]
-      
-      dt_chunk <- melt(dt_chunk, measure.vars = sort(ELEC_DIMS))
-      
-      dt_chunk[ , variable := gsub("Elec Median ", "", variable) %>% as.numeric()]
-      
-      dt_chunk[ , value := as.numeric(value)]
     }
 
+    dt_chunk <- melt(dt_chunk, id.vars = c("Attribute 1", "Attribute 2"))
+    dt_chunk[ , variable := gsub("Gas Median |Elec Median", "", variable) %>% as.numeric()]
+    dt_chunk[ , value := as.numeric(value)]
+    
     return(dt_chunk)
   })
   
   
+  
   output$plot1 <- renderPlotly({
-    
-    
+
     s_data_copy <- selectedData()
     s_data_copy[ , date := as.POSIXct(as.Date(paste(variable, "01", "01", sep = "-")))]
-    
     
     forecast_collector <- data.table()
     for (build_age in unique(s_data_copy$`Attribute 2`)) {
@@ -112,8 +103,6 @@ server <- function(input, output, session) {
       m <- prophet(history)
       future <- make_future_dataframe(m, periods = 2, freq='year')
       forecast <- predict(m, future)
-      
-      # TOOD: ADD 
       
       s_data_copy_subset <- merge(s_data_copy_subset,
                                   forecast[ , c("ds", "yhat", "yhat_lower", "yhat_upper")],
@@ -134,10 +123,6 @@ server <- function(input, output, session) {
                                id.vars = c("date", "Attribute 1", "Attribute 2"), 
                                measure.vars = c("value", "yhat"))
 
-    
-    # browser()
-
-
     forecast_collector[ , `Attribute 2` := recode(`Attribute 2`, 
                                                   `Pre 1919` = "6. Oldest buildings: Pre 1919",
                                                   `1919-44` = "5. Older buildings: 1919-44", 
@@ -150,10 +135,9 @@ server <- function(input, output, session) {
                                                   levels = sort(unique(`Attribute 2`)),
                                                   ordered = TRUE)]
     forecast_collector[ , variable := recode(variable, 
-                                                  `value` = "(actuals)",
-                                                  `yhat` = "(predicted)")]
+                                             `value` = "(actuals)",
+                                             `yhat` = "(predicted)")]
     
-    # browser()
     plot_ly(data = forecast_collector, 
             x = ~date,
             y = ~value,
@@ -161,19 +145,19 @@ server <- function(input, output, session) {
             linetype = ~variable,
             color = ~`Attribute 2`) %>%
       add_lines(
-                type = 'scatter', 
-                mode = "lines+markers",
-                line = list(width = 4)
-                ) %>%
+        type = 'scatter', 
+        mode = "lines+markers",
+        line = list(width = 4)
+      ) %>%
       layout(title = 'Forecasted vs actual energy usage at yearly level',
              legend = list(orientation = "h",   # show entries horizontally
                            xanchor = "left",  # use center of legend as anchor
                            y = 0.7,
                            x = 1
-                           ),
+             ),
              xaxis = list(title = 'Year'),
              yaxis = list(title = 'Median energy usage')
-             )
+      )
     
   })
   
